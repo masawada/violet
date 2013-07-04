399dcd30e41bab72202cda956090cf1e7a062b17
Masayoshi Wada <developer@andantesoftware.com>
2013-07-05 00:48:58 +0900

    util.js and oauth.js

--- src/util.js
+++ src/util.js
@@ -6,3 +6,17 @@ Violet.Util.uri_encode = function(text){
   return encodeURIComponent(text).replace(/[!'()]/g, escape).replace(/\*/g, "%2A");
 };
 
+Violet.Util.mergeHash = function(){
+  var l = arguments.length, i, key, result = {};
+  for(i=0;i<l;i++){
+    var hash = arguments[i];
+    for(key in hash){
+      if(hash.hasOwnProperty(key)){
+        result[key] = hash[key];
+      }
+    }
+  }
+
+  return result;
+};
+
