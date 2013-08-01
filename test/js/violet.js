312a07ed1fd56c3be77ec8692ca6b793a79e85d0
Masayoshi Wada <developer@andantesoftware.com>
2013-08-02 01:07:33 +0900

    fix #3
    add 'this'

--- test/js/violet.js
+++ test/js/violet.js
@@ -550,10 +550,10 @@ var proto = Violet.init.prototype;
 
 proto.initialize = function(){
   this.oauth = Violet.OAuth({
-    consumer_key: consumer_key,
-    consumer_secret: consumer_secret,
-    access_token: access_token,
-    access_token_secret: access_token_secret
+    consumer_key: this.consumer_key,
+    consumer_secret: this.consumer_secret,
+    access_token: this.access_token,
+    access_token_secret: this.access_token_secret
   });
 
   this.streaming = Violet.Streaming(this.oauth);
