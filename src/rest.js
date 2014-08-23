59c81706d5722d52fab16a14239c14ebf4829a42
tyage <namatyage@gmail.com>
2014-08-23 22:00:46 +0900

    rest api returns proper object

--- src/rest.js
+++ src/rest.js
@@ -3,7 +3,13 @@
 //
 
 (function(Violet) {
-  var util = Violet.Util;
+  var Util = Violet.Util;
+  var Tweet = Violet.Tweet;
+  var tweetStream = function(tweets) {
+    return tweets.map(function(tweet) {
+      return new Tweet(tweet);
+    });
+  };
 
   var Rest = function(oauth) {
     this.oauth = oauth;
@@ -26,7 +32,7 @@
       retweeterIds: 'statuses/retweeters/ids',
     },
     _getRequestURI: function(endpoint, params) {
-      var path = util.resolveEndpoint(endpoint, params);
+      var path = Util.resolveEndpoint(endpoint, params);
       return this.apiBaseURI + path;
     },
     _getOAuthedRequest: function(method, uri, data) {
@@ -38,82 +44,105 @@
       xhr.setOAuthHeader(this.oauth.obtainOAuthParams(xhr));
       return xhr;
     },
-    _promiseXHR: function(xhr) {
+    _promiseXHR: function(xhr, callback) {
       xhr.start();
       var promise = new Promise();
       xhr.addEventListener('load', function() {
-        // XXX generate tweet objects...
-        promise.resolve(new TweetObjects(this.result));
+        promise.resolve(callback(JSON.parse(this.result)));
       });
       return promise;
     },
     getMentionsTimeline: function(params) {
       var uri = this._getRequestURI(this.endpoints.mentionsTimeline);
       var xhr = this._getOAuthedRequest('GET', uri, params);
-      return this._promiseXHR(xhr);
+      return this._promiseXHR(xhr, function(result) {
+        return tweetStream(result);
+      });
     },
     getUserTimeline: function(params) {
       var uri = this._getRequestURI(this.endpoints.userTimeline);
       var xhr = this._getOAuthedRequest('GET', uri, params);
-      return this._promiseXHR(xhr);
+      return this._promiseXHR(xhr, function(result) {
+        return tweetStream(result);
+      });
     },
     getHomeTimeline: function(params) {
       var uri = this._getRequestURI(this.endpoints.homeTimeline);
       var xhr = this._getOAuthedRequest('GET', uri, params);
-      return this._promiseXHR(xhr);
+      return this._promiseXHR(xhr, function(result) {
+        return tweetStream(result);
+      });
     },
     getRetweetsOfMe: function(params) {
       var uri = this._getRequestURI(this.endpoints.retweetsOfMe);
       var xhr = this._getOAuthedRequest('GET', uri, params);
-      return this._promiseXHR(xhr);
+      return this._promiseXHR(xhr, function(result) {
+        return tweetStream(result);
+      });
     },
     getRetweets: function(statusId, params) {
       var uri = this._getRequestURI(this.endpoints.retweets, {
         id: statusId
       });
       var xhr = this._getOAuthedRequest('GET', uri, params);
-      return this._promiseXHR(xhr);
+      return this._promiseXHR(xhr, function(result) {
+        return tweetStream(result);
+      });
     },
     showStatus: function(statusId, params) {
       var uri = this._getRequestURI(this.endpoints.showStatus, {
         id: statusId
       });
       var xhr = this._getOAuthedRequest('GET', uri, params);
-      return this._promiseXHR(xhr);
+      return this._promiseXHR(xhr, function(result) {
+        return new Tweet(result);
+      });
     },
     destroyStatus: function(statusId, params) {
       var uri = this._getRequestURI(this.endpoints.destroyStatus, {
         id: statusId
       });
       var xhr = this._getOAuthedRequest('POST', uri, params);
-      return this._promiseXHR(xhr);
+      return this._promiseXHR(xhr, function(result) {
+        return new Tweet(result);
+      });
     },
     updateStatus: function(params) {
       var uri = this._getRequestURI(this.endpoints.updateStatus);
       var xhr = this._getOAuthedRequest('POST', uri, params);
-      return this._promiseXHR(xhr);
+      return this._promiseXHR(xhr, function(result) {
+        return new Tweet(result);
+      });
     },
     retweetStatus: function(statusId, params) {
       var uri = this._getRequestURI(this.endpoints.retweetStatus, {
         id: statusId
       });
       var xhr = this._getOAuthedRequest('POST', uri, params);
-      return this._promiseXHR(xhr);
+      return this._promiseXHR(xhr, function(result) {
+        return new Tweet(result);
+      });
     },
     updateStatusWithMedia: function(params) {
       var uri = this._getRequestURI(this.endpoints.updateStatusWithMedia);
       var xhr = this._getOAuthedRequest('POST', uri, params);
-      return this._promiseXHR(xhr);
+      return this._promiseXHR(xhr, function(result) {
+        return new Tweet(result);
+      });
     },
     getOEmbed: function(params) {
       var uri = this._getRequestURI(this.endpoints.oEmbed);
       var xhr = this._getOAuthedRequest('GET', uri, params);
-      return this._promiseXHR(xhr);
+      return this._promiseXHR(xhr, function(result) {
+        return result;
+      });
     },
     getRetweeterIds: function(params) {
       var uri = this._getRequestURI(this.endpoints.getRetweeterIds);
       var xhr = this._getOAuthedRequest('GET', uri, params);
-      return this._promiseXHR(xhr);
+      return this._promiseXHR(xhr, function(result) {
+        return result;
+      });
     }
   };
 
