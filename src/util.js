d91ec058ae19c87c361216a6f291cfef3d79e7fc
Masayoshi Wada <developer@andantesoftware.com>
2014-08-23 20:44:15 +0900

    add mergeMaps method

--- src/util.js
+++ src/util.js
@@ -7,6 +7,22 @@
     return encodeURIComponent(text).replace(/[!'()]/g, escape).replace(/\*/g, "%2A");
   };
 
+  Util.mergeMaps = function () {
+    var dst = arguments[0];
+    var i, j, src, keys;
+
+    for (i = 1; i < arguments.length; i++) {
+      src = arguments[i];
+      keys = Object.keys(src);
+
+      for (j = 0; j < keys.length; j++) {
+        dst[keys[j]] = src[keys[j]];
+      }
+    }
+
+    return dst;
+  };
+
   Util.resolveEndpoint = function(endpoint, params) {
     if (params === undefined) {
       params = {};
