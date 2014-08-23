19fba56921ad070d319d17469f67812c9424b975
tyage <namatyage@gmail.com>
2014-08-23 15:24:50 +0900

    sample violet init

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@
 // violet.js
 //
 
-var Violet = function () {
+var Violet = function (oauth) {
+  this.oauth = new Violet.OAuth(oauth);
+  this.rest = new Violet.Rest(this.oauth);
+  this.streaming = new Violet.Streaming(this.oauth);
 };
 
 Violet.prototype = {
