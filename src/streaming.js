0e3f707144ae47310c7fa49edf67de7f24d0c5a5
Masayoshi Wada <developer@andantesoftware.com>
2014-08-24 15:03:45 +0900

    add endpoints and apiBaseURI

--- src/streaming.js
+++ src/streaming.js
@@ -10,6 +10,14 @@
   };
 
   Streaming.prototype = {
+    apiBaseURI: 'https://stream.twitter.com/1.1/',
+    endpoints: {
+      filterStream: 'statuses/filter.json',
+      sampleStream: 'statuses/sample.json',
+      firehose: 'statuses/firehose.json',
+      userStream: 'https://userstream.twitter.com/1.1/user.json',
+      siteStream: 'https://sitestream.twitter.com/1.1/site.json'
+    }
   };
   Violet.Streaming = Streaming;
 })(Violet);
