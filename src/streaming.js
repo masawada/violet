4dddeb4f941ff59b10d41fd3f772f1599f60cd20
Masayoshi Wada <developer@andantesoftware.com>
2014-08-24 14:56:27 +0900

    add streaming.js

---
+++ src/streaming.js
@@ -0,0 +1,15 @@
+//
+// Violet.Streaming
+//
+
+(function(Violet) {
+  var Util = Violet.Util;
+
+  var Streaming = function(oauth) {
+    this.oauth = oauth;
+  };
+
+  Streaming.prototype = {
+  };
+  Violet.Streaming = Streaming;
+})(Violet);
