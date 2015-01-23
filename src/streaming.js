0c9c6ec44f6612af1d5c820de705ac706163ce6f
Masayoshi Wada <developer@andantesoftware.com>
2015-01-23 23:32:03 +0900

    implement addEventListener method

--- src/streaming.js
+++ src/streaming.js
@@ -7,6 +7,7 @@
 
   var Streaming = function(oauth) {
     this.oauth = oauth;
+    this._events = {};
   };
 
   Streaming.prototype = {
@@ -17,6 +18,9 @@
       firehose: 'statuses/firehose.json',
       userStream: 'https://userstream.twitter.com/1.1/user.json',
       siteStream: 'https://sitestream.twitter.com/1.1/site.json'
+    },
+    on: function(key, callback) {
+      this._events[key] = callback;
     }
   };
   Violet.Streaming = Streaming;
