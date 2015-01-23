ba6e6be38e3ce673d89d854785f209d086932036
Masayoshi Wada <developer@andantesoftware.com>
2015-01-23 13:56:34 +0900

    create account.js

---
+++ src/account.js
@@ -0,0 +1,30 @@
+//
+// Violet.Accounts
+//
+
+(function(Violet) {
+  var Accounts = function() {
+    this._accounts = {};
+  };
+
+  Accounts.prototype = {
+    requestAuthorizeURI: function() {
+    },
+    addWithPIN: function(pin) {
+    },
+    getList: function() {
+    },
+    get: function(accountId) {
+    },
+    setPrimary: function(accountId) {
+    },
+    getPrimary: function() {
+    },
+    getOAuthManager: function() {
+    },
+    remove: function(accountId) {
+    }
+  };
+
+  Violet.Accounts = Accounts;
+})(Violet);
