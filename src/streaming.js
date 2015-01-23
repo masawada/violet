e664b1e1f6ef1a520b762b1442692803f07c74de
Masayoshi Wada <developer@andantesoftware.com>
2015-01-24 00:06:12 +0900

    implement start/stop filter/user Stream methods

--- src/streaming.js
+++ src/streaming.js
@@ -8,27 +8,67 @@
   var Streaming = function(accounts) {
     this.accounts = accounts;
     this._events = {};
+    this._connections = {
+      'userStream': {},
+      'filterStream': {}
+    };
   };
 
   Streaming.prototype = {
     apiBaseURI: 'https://stream.twitter.com/1.1/',
     endpoints: {
-      filterStream: 'statuses/filter.json',
-      sampleStream: 'statuses/sample.json',
-      firehose: 'statuses/firehose.json',
-      userStream: 'https://userstream.twitter.com/1.1/user.json',
-      siteStream: 'https://sitestream.twitter.com/1.1/site.json'
+      userStream: {
+        method: 'GET',
+        path: 'https://userstream.twitter.com/1.1/user.json'
+      },
+      filterStream: {
+        method: 'POST',
+        path: 'statuses/filter.json'
+      },
     },
     on: function(key, callback) {
       this._events[key] = callback;
     },
     startUserStream: function(accountId) {
+      var method = this.endpoints.userStream.method;
+      var uri = this.endpoints.userStream.path;
+
+      if (!this._connections.userStream[accountId]) {
+        this._connections.userStream[accountId] = this._startStreaming(accountId, method, uri);
+      }
     },
     stopUserStream: function(accountId) {
+      this._connections.userStream[accountId].stop();
+      delete this._connections.userStream[accountId];
     },
     startFilterStream: function(accountId) {
+      var method = this.endpoints.filterStream.method;
+      var uri = this.apiBaseURI + this.endpoints.filterStream.path;
+
+      if (!this._connections.filterStream[accountId]) {
+        this._connections.filterStream[accountId] = this._startStreaming(accountId, method, uri);
+      }
     },
     stopFilterStream: function(accountId) {
+      this._connections.filterStream[accountId].stop();
+      delete this._connections.filterStream[accountId];
+    },
+    _startStreaming: function(accountId, method, uri) {
+      var oauth = this.accounts.getOAuthManager(accountId);
+      var conn = new Violet.HTTPClient({
+        method: method,
+        uri: uri
+      });
+
+      conn.setOAuthHeader(oauth.obtainOAuthParams(method, uri));
+      conn.addEventListener('progress', function(event) {
+        this._parseStream(accountId, event);
+      }.bind(this));
+
+      conn.start();
+      return conn;
+    },
+    _parseStream: function(accountId, event) {
     }
   };
   Violet.Streaming = Streaming;
