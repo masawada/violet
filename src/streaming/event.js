a192cd47803883a6806669ce872c3dae2ac6f213
Masayoshi Wada <developer@andantesoftware.com>
2015-01-24 14:26:48 +0900

    fix wrong args

--- src/streaming/event.js
+++ src/streaming/event.js
@@ -3,11 +3,20 @@
 //
 
 (function(Violet) {
+  var listEvents = ['list_created', 'list_destroyed', 'list_updated', 'list_member_added', 'list_member_removed', 'list_user_subscribed', 'list_user_unsubscribed'];
+  var tweetEvents = ['favorite', 'unfavorite'];
+
   var Event = function(data, accounts, accountId) {
     var type = data.event;
-    var source = new Violet.User(data, accounts, accountId);
-    var target = new Violet.User(data, accounts, accountId);
-    var targetObject = new Violet.Tweet(data, accounts, accountId);
+    var source = new Violet.User(data.source, accounts, accountId);
+    var target = new Violet.User(data.target, accounts, accountId);
+
+    var targetObject = null;
+    if (listEvents.indexOf(type) !== -1) {
+      targetObject = null;
+    } else if (tweetEvents.indexOf(type) !== -1) {
+      targetObject = new Violet.Tweet(data.target_object, accounts, accountId);
+    }
 
     return {
       type: type,
