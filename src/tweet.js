e57fb21b19250dfc8af058af22c5466c8021b284
Masayoshi Wada <developer@andantesoftware.com>
2015-01-25 22:46:42 +0900

    set tweet attributes

--- src/tweet.js
+++ src/tweet.js
@@ -3,10 +3,20 @@
 //
 
 (function(Violet) {
-  var Tweet = function(data, accounts, accountId) {
-    this.rawData = data;
+  var Tweet = function(tweetData, accounts, accountId) {
     this.accounts = accounts;
     this._accountId = accountId;
+    this._initWithTweet(tweetData);
+  };
+
+  Tweet.prototype = {
+    _initWithTweet: function(tweet){
+      var attributes = [];
+
+      attributes.forEach(function(attribute) {
+        this[attribute] = tweet[attribute] || null;
+      }.bind(this));
+    }
   };
 
   Violet.Tweet = Tweet;
