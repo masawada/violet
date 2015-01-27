fed01a6a6bafabe39aee8e5fa1fa781237de758b
Masayoshi Wada <developer@andantesoftware.com>
2015-01-27 23:58:49 +0900

    check retweet

--- src/tweet.js
+++ src/tweet.js
@@ -11,11 +11,32 @@
 
   Tweet.prototype = {
     _initWithTweet: function(tweet){
-      var attributes = [];
+      var tweetData;
+      var attributes = ["text", "truncated", "in_reply_to_status_id", "in_reply_to_status_id_str", "in_reply_to_user_id", "in_reply_to_user_id_str", "in_reply_to_screen_name", "geo", "coordinates", "place", "contributors", "retweet_count", "favorite_count", "entities", "favorited", "retweeted", "lang"];
 
+      if (tweet.retweeted_status) {
+        this.retweeted_status = true;
+        this.retweeted_id = tweet.id_str;
+        this.retweeted_user = new Violet.User(tweet.user, this.accounts, this._accountId);
+        this.retweeted_at = new Date(tweet.created_at);
+        tweetData = tweet.retweeted_status;
+      } else {
+        this.retweeted_status = false;
+        tweetData = tweet;
+      }
+
+      // register attributes
       attributes.forEach(function(attribute) {
         this[attribute] = tweet[attribute] || null;
       }.bind(this));
+
+      this.id = tweetData.id_str;
+      this.user = new Violet.User(tweetData.user, this.accounts, this._accountId);
+      this.created_at = new Date(tweetData.created_at);
+      this.source = {
+        name: tweetData.source.match(/<a.*>(.*)<\/a>/)[1],
+        url: tweetData.source.match(/<a\shref=\"(.*?)\"\srel=\"nofollow\">.*<\/a>/)[1]
+      };
     }
   };
 
