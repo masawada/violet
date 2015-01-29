17c7b6b2d481db9ef665295808402e4dbe29f58c
Masayoshi Wada <developer@andantesoftware.com>
2015-01-29 13:35:15 +0900

    add rest.js

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@
 
 var Violet = function (keys) {
   this.accounts = new Violet.Accounts(keys);
+  this.rest = new Violet.REST(this.accounts);
   this.streaming = new Violet.Streaming(this.accounts);
 };
---
+++ src/rest/rest.js
@@ -0,0 +1,11 @@
+//
+// Violet.REST
+//
+
+(function(Violet) {
+  var REST = function(accounts) {
+    this._accounts = accounts;
+  };
+
+  Violet.REST = REST;
+})(Violet);
